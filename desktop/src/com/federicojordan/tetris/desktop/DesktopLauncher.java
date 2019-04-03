34e25d774351fa2eebf8d3a5f58547e6c92cf040
OLX - Federico Jordan <federico.jordan@olx.com>
2019-04-03 00:57:27 -0300

    First commit - LibGDX installation

---
+++ desktop/src/com/federicojordan/tetris/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.federicojordan.tetris.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.federicojordan.tetris.LibGDXTetris;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LibGDXTetris(), config);
+	}
+}
---
+++ ios/src/com/federicojordan/tetris/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.federicojordan.tetris;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.federicojordan.tetris.LibGDXTetris;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new LibGDXTetris(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
